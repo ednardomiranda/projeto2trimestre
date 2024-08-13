788db9af8ee419808729575613db159d96f20861
calango <110404322+ednardomiranda@users.noreply.github.com>
2024-08-13 16:38:43 -0300

    criamos as perguntas

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-princpal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+         alternativas:[
+            "Alternativa 1",
+            "Alterntiva 2"
+         ]
+    },
+{
+    anunciado: "Pergunta 2"
+    altenativa:[
+        "Alternstiva 1"
+        "Alternativa 2"
+    ]
+}
+]
